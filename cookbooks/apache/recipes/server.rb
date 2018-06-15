53aeb4ae608c5b008052a4b282a14673109bd579
vagrant <vagrant@localhost.localdomain>
2018-06-15 14:45:37 +0000

    adding apache cookbook

---
+++ cookbooks/apache/recipes/server.rb
@@ -0,0 +1,11 @@
+package 'httpd' do
+  action :install
+end
+
+file 'var/www/html/index.html' do
+  content '<html><h1>Hello,World</h1></html>'
+end
+
+service 'httpd' do
+  action [ :enable, :start ]
+end
