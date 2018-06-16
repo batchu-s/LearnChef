d1537f436210e31f69c5d439ec973e0405b00bb3
vagrant <vagrant@localhost.localdomain>
2018-06-16 16:52:29 +0000

    version 0.1.2 - adding template to server.rb index.html.erb

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -2,13 +2,12 @@ package 'httpd' do
   action :install
 end
 
-file 'var/www/html/index.html' do
-  content "<html><h1>Hello,World</h1>
-  HOSTNAME: #{node['hostname']}<br/>
-  IPADDRESS: #{node['ipaddress']}<br/>
-  CPU: #{node['cpu']['0']['mhz']}<br/>
-  MEMORY: #{node['memory']['total']}
-</html>"
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  variables(
+    :name => 'Sumanth Kumar'
+  )
+  action :create
 end
 
 service 'httpd' do
