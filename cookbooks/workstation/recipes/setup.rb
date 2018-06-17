66c6cf5261c4efe7c3c6ce2baa2d8be508bb253f
vagrant <vagrant@localhost.localdomain>
2018-06-17 02:51:01 +0000

    Adding an user and a group using user and group resource

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -16,3 +16,15 @@ service 'ntpd' do
   action [ :enable, :start ]
 end
 
+user 'suresh' do
+  comment 'suresh'
+  uid '134'
+  shell '/bin/bash'
+  home '/home/suresh'
+  manage_home true
+end
+
+group 'test' do
+  members 'suresh'
+  append true
+end
